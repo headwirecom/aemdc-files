30b49f4ec7da12311fca6b3093c1def24a536eeb
Marat Saitov <github@ceridan.de>
2016-11-22 20:17:55 +0100

    Fix for heroJS to show the text, simplifying and targetname for contentpage comp

--- placeholders/content/src/main/content/jcr_root/apps/project/components/heroJS/{{targetname}}.js
+++ placeholders/content/src/main/content/jcr_root/apps/project/components/heroJS/{{targetname}}.js
@@ -8,13 +8,16 @@ use(function () {
 
     var compObj = {};
 
-    // The actual "hero" content
-    compObj.hero = granite.resource.properties[CONST.PROP_HERO]
-            || resourcePage.getProperties().get(CONST.PROP_HERO);
+    var heroProp = granite.resource.properties[CONST.PROP_HERO];;
+    if (!heroProp) {
+        compObj.hero = "No text saved";
+    } else {
+        // The actual "hero" content
+        compObj.hero = heroProp;
+    }
 
     // Adding the constants to the exposed API
     compObj.CONST = CONST;
 
     return compObj;
-
 });

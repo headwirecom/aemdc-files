fe39471ac499ea720f57bce3ea586b18138bf1e0
Marat Saitov <github@ceridan.de>
2016-11-23 17:27:43 +0100

    Placeholders in the HTML and instead of dialog fields

--- placeholders/content/src/main/content/jcr_root/apps/project/components/component-js/{{targetname}}.js
+++ placeholders/content/src/main/content/jcr_root/apps/project/components/component-js/{{targetname}}.js
@@ -2,22 +2,10 @@
 
 use(function () {
 
-    var CONST = {
-        PROP_HERO: "hero"
-    }
-
     var compObj = {};
 
-    var heroProp = granite.resource.properties[CONST.PROP_HERO];;
-    if (!heroProp) {
-        compObj.hero = "No text saved";
-    } else {
-        // The actual "hero" content
-        compObj.hero = heroProp;
-    }
-
-    // Adding the constants to the exposed API
-    compObj.CONST = CONST;
+    // Add your code here.
+    compObj.text = "No text saved";
 
     return compObj;
 });
